77ceedb9d4a9678f851027f11c664448b3ebf437
Nick <nick22783@gmail.com>
2024-01-29 14:35:27 +0300

    create the second paddle

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ screen.setup(width=800, height=600)
 screen.title("Pong Game")
 screen.tracer(0)
 
-paddle = Paddle()
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkeypress(paddle.up, 'Up')
-screen.onkeypress(paddle.down, 'Down')
+screen.onkeypress(right_paddle.up, 'Up')
+screen.onkeypress(right_paddle.down, 'Down')
+screen.onkeypress(left_paddle.up, 'w')
+screen.onkeypress(left_paddle.down, 's')
 
 game_is_on = True
 
