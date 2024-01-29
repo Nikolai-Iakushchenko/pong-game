9b735d0f582a4f3501515f6b41626a4a2f883167
Nick <nick22783@gmail.com>
2024-01-29 15:38:32 +0300

    add the ball bouncing logic

--- ball.py
+++ ball.py
@@ -10,10 +10,15 @@ class Ball(Turtle):
         self.up()
         self.speed(0)
         self.lt(20)
+        self.x_direction = 10
+        self.y_direction = 10
 
     def move(self):
-        # if not(self.xcor() > 380 or self.xcor() < - 380 or self.ycor() > 280 or self.ycor() < -280):
-        #     self.fd(1)
-        nex_x = self.xcor() + 10
-        nex_y = self.ycor() + 10
+        if self.xcor() == 380 or self.xcor() == - 380:
+            self.x_direction = - self.x_direction
+        elif self.ycor() == 280 or self.ycor() == -280:
+            self.y_direction = - self.y_direction
+
+        nex_x = self.xcor() +  self.x_direction
+        nex_y = self.ycor() +  self.y_direction
         self.goto(nex_x, nex_y)
